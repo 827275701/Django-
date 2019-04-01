2024855184028407b2eb9a22b0de63ceea8fcc66
lhwei <827275701@qq.com>
2019-04-01 20:45:34 +0800

    第一个项目

---
+++ mysite/cmdb/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+# Create your views here.
+
+
+
+def index(request):
+    #return HttpResponse("hello world")
+    #return render(request, "index.html",)
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        print(username, password)
+    return render(request, "index.html",)
