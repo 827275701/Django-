8492ec118935a56d43053ed5f974c26c82a3c267
lhwei <l>
2019-04-23 15:49:54 +0800

    update

--- mysite/android/urls.py
+++ mysite/android/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     url('admin', views.admin_index),
+    url('login', views.android_login),
+    url('choose_spot', views.android_choose_spot),
+    url('now_data', views.android_now_data),
+    url('person_info', views.android_person_info),
 
 ]
--- mysite/android/views.py
+++ mysite/android/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 def admin_index(request):
 
     return render(request, "admin/admin.html", )
+
+def android_login(request):
+    return HttpResponse('yes')
+
+def android_choose_spot(request):
+    return HttpResponse('name=xxx&number=001')
+
+def android_now_data(request):
+    return HttpResponse('wendu=24&shidu=37&qiti=2.5')
+
+def android_person_info(request):
+    return HttpResponse('lhwie&001&20&man&2015-2-14&1777-10-21&xxxxxxxxx')
+
