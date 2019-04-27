69420abbe17ae3e3832bb2678f90c533743fe9ca
lhwei <l>
2019-04-27 22:25:13 +0800

    update

--- mysite/android/urls.py
+++ mysite/android/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url('t_history', views.android_t_history),
     url('h_history', views.android_h_history),
     url('g_history', views.android_g_history),
+    url('add_user', views.android_g_history),
+
 ]
