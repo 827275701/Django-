ce8441937c3ac5156006bb6318a89faaee97e239
lhwei <l>
2019-04-29 12:26:12 +0800

    Signed-off-by: lhwei <l>

--- mysite/android/urls.py
+++ mysite/android/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     url('choose_spot', views.android_choose_spot),
     url('now_data', views.android_now_data),
     url('person_info', views.android_person_info),
-    url('history', views.android_t_history),
-    url('add_user', views.android_g_history),
-
+    url('history', views.android_history),
 ]
