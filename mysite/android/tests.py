983a5c24f4269fae63b4bf05e2d76a08d4f5a483
lhwei <l>
2019-05-13 19:33:29 +0800

    update

--- mysite/android/tests.py
+++ mysite/android/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+
+class SimpleTest(TestCase):
+    def test_login(self):
+        c = Client(HTTP_USER_AGENT='Mozilla/5.0')
+        response = c.post('/android/login/',
+                          {'username': 'admin', 'password': 'admin'})
+        self.assertEqual(response.status_code, 200)
--- mysite/android/views.py
+++ mysite/android/views.py
@@ -8,16 +8,16 @@ def android_login(request):
     return HttpResponse('yes')
 
 def android_choose_spot(request):
-    return HttpResponse('name=xxx&number=001')
+    return HttpResponse('lhw&001')
 
 def android_now_data(request):
-    return HttpResponse('wendu=24&shidu=37&qiti=2.5')
+    return HttpResponse('24&s37&2.1')
 
 def android_person_info(request):
-    return HttpResponse('lhwie&001&20&man&2015-2-14&1777-10-21&xxxxxxxxx')
+    return HttpResponse('lhwie&001&20&man&18165151223')
 
 def android_history(request):
-    return HttpResponse('test_history')
+    return HttpResponse('20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20&20')
 
 def android_add_user(request):
     return HttpResponse("test_add_user")
